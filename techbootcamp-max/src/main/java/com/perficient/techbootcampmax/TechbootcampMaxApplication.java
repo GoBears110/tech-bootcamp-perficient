0fd15431c64b085031e4d71ad9356f7f5fe0be87
MaxK <maxkleinerman@gmail.com>
2022-01-17 14:10:26 -0600

    Generated Spring Boot

---
+++ techbootcamp-max/src/main/java/com/perficient/techbootcampmax/TechbootcampMaxApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampMaxApplication.class, args);
+	}
+
+}
---
+++ techbootcamp-max/src/test/java/com/perficient/techbootcampmax/TechbootcampMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbootcampMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
